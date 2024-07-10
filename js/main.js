21470cacef5c0428b5d14d24b50f02cd8fa8805a
satorumaruyama <satolale77@gmail.com>
2024-07-10 22:35:03 +0900

    Lesson8_初稿

--- js/main.js
+++ js/main.js
@@ -5,3 +5,15 @@ $(function () {
       $('.header__hamburger').toggleClass('header__hamburger-open')
     });
   });
+
+// アコーディオン
+  $('.faq__answer').hide();
+  $('.faq__item').click(function() {
+      var answer = $(this).find('.faq__answer');
+      answer.slideToggle(function() {
+          if (answer.is(':visible')) {
+              answer.css('width', '90%');
+              answer.css('margin', '0 auto');
+          }
+      });
+  });
